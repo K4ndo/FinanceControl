1aab216125a94b0c269a8ff74212207a91038807
Dennis Kessler <dennis.kessler91@gmail.com>
2019-03-25 22:40:19 +0100

    still trying on tkinter:

--- gui.py
+++ gui.py
@@ -1,25 +1,7 @@
-from tkinter import*
-from tkinter import filedialog
-# def show_entry_fields():
-#   print("First Name: %s\nLast Name: %s" % (e1.get(), e2.get()))
+import tkinter
+import tkinter.filedialog
 
-'''
-def show_entry_fields():
-    print("First Name: %s\nLast Name: %s" % (e1.get(), e2.get()))
-    e1.delete(0, END)
-    e2.delete(0, END)
-'''
-
-
-def NewFile():
-    print("New File!")
-
-
-def OpenFile():
-    print("name")
-
-
-fields = "Vorname", "Nachname", "Kontoauszug"
+fields = {"a", "b", "c"}
 
 
 def fetch(entries):
@@ -45,7 +27,9 @@ def makeform(root, fields):
 
         if field is fields[2]:
             # entries.append((field, ent))
+            lab.setvar("test", 1)
             b1.pack(expand=YES, fill=X)
+
         entries.append((field, ent))
 
     return entries
@@ -61,67 +45,24 @@ def openFileDiaglogue():
     if file != None:
         data = file.read()
         file.close()
+
         print("I got %d bytes from this file." % len(data))
 
 
 if __name__ == '__main__':
-    master = Tk()
+    master = tkinter.Tk()
     master.geometry("500x500")
     ents = makeform(master, fields)
     #master.bind('<Return>', (lambda event, e=ents: fetch(e)))
-    pathlabel = Label(master)
+    pathlabel = tkinter.Label(master)
 
-    b1 = Button(master, text='Show',
-                command=(lambda e=ents: fetch(e)))
-    b1.pack(side=LEFT, padx=5, pady=5)
-    b2 = Button(master, text='Quit', command=master.quit)
-    b2.pack(side=LEFT, padx=5, pady=5)
-    b3 = Button(master, text='browse', command=openFileDiaglogue)
-    b3.pack(side=LEFT, padx=5, pady=5)
-
-
-'''
-root.mainloop()
-'''
-'''
-master = Tk()
-master.title("Finance Controle")
-menubar = Menu(master)
-master.config(menu=menubar)
-filemenu = Menu(menubar)
-'''
-
-'''
-def show_entry_fields():
-    print("First Name: %s\nLast Name: %s" % (e1.get(), e2.get()))
-'''
-
-'''
-filemenu = Menu(menubar, tearoff=0)
-menubar.add_cascade(label="File", menu=filemenu)
-filemenu.add_command(label="Speichtern unter", command=NewFile())
-# filemenu.add_separator()
-filemenu.add_command(label="Programm beenden", command=OpenFile())
-master.geometry("400x200")
-
-Label(master, text="First Name").grid(row=0)
-Label(master, text="Last Name").grid(row=1)
-Label()
-e1 = Entry(master)
-e2 = Entry(master)
-
-e1.grid(row=0, column=1)
-e2.grid(row=1, column=1)
-test = e1.get()
-
-Button(master, text='Quit', command=master.quit).grid(
-    row=3, column=0, sticky=W, pady=4)
-
-Button(master, text='Show', command=openFileDiaglogue()).grid(
-    row=3, column=1, sticky=W, pady=4)
-'''
-
-#Button(master, text='Show', command=openFileDiaglogue())
+    b1 = tkinter.Button(master, text='Show',
+                        command=(lambda e=ents: fetch(e)))
+    b1.pack(side=tkinter.LEFT, padx=5, pady=5)
+    b2 = tkinter.Button(master, text='Quit', command=master.quit)
+    b2.pack(side=tkinter.LEFT, padx=5, pady=5)
+    b3 = tkinter.Button(master, text='browse', command=openFileDiaglogue)
+    b3.pack(side=tkinter.LEFT, padx=5, pady=5)
 
 
 master.mainloop()
---
+++ guiHannes.py
@@ -0,0 +1,66 @@
+# from tkinter import* # FIXME keine Wildcard imports
+import tkinter
+import tkinter.filedialog
+
+
+def _print_entered_data(field_names_and_entries):
+    for field_name in field_names_and_entries.keys():
+        text = field_names_and_entries[field_name].get()
+        print('{}: "{}"'.format(field_name, text))
+
+
+def _create_entry_row(root, name, with_browse_button=False):
+    frame = tkinter.Frame(root)
+    frame.pack(side=tkinter.TOP, fill=tkinter.X, padx=10, pady=10)
+
+    field_name_label = tkinter.Label(frame, width=25, text=name, anchor='w', justify=tkinter.LEFT)
+    field_name_label.pack(anchor=tkinter.W, side=tkinter.LEFT)
+
+    entry = tkinter.Entry(frame, justify=tkinter.LEFT)
+    entry.pack(anchor=tkinter.W, side=tkinter.LEFT, fill=tkinter.X)
+
+    if with_browse_button:
+        browse_button = tkinter.Button(frame, text='browse', command=_open_file_dialog)
+        browse_button.pack(anchor=tkinter.W)
+    return entry
+
+
+def _make_form(root):
+    field_names_and_entries = {}
+    first_name = "Vorname"
+    field_names_and_entries[first_name] = _create_entry_row(root, first_name)
+    last_name = "Nachname"
+    field_names_and_entries[last_name] = _create_entry_row(root, last_name)
+    statement_path = "Kontoauszug"
+    field_names_and_entries[statement_path] = _create_entry_row(root, statement_path, with_browse_button=True)
+    return field_names_and_entries
+
+
+def _open_file_dialog():
+    # FIXME Niemals eine Variable "file" nennen (ist ein Python-Keyword)
+    chosen_file = tkinter.filedialog.askopenfile(parent=master, mode='rb', title='Choose a file')
+    pathlabel.config(text=chosen_file)
+    pathlabel.pack()
+    if chosen_file:
+        print(chosen_file.name)
+        # FIXME data sollte dann wohl in eine globale Variable, oder (wenn du das ganze in einer Klasse schreibst) in eine Klassenvariable
+        data = chosen_file.read()
+        chosen_file.close()
+        print("I got %d bytes from this file." % len(data))
+
+
+if __name__ == '__main__':
+    master = tkinter.Tk()
+    master.geometry("500x200")
+
+    entries = _make_form(master)
+    pathlabel = tkinter.Label(master)
+
+    b1 = tkinter.Button(master, text='Show', command=(lambda e=entries: _print_entered_data(e)))
+    b1.pack(side=tkinter.LEFT, padx=5, pady=5)
+    b2 = tkinter.Button(master, text='Quit', command=master.quit)
+    b2.pack(side=tkinter.LEFT, padx=5, pady=5)
+    b3 = tkinter.Button(master, text='browse', command=_open_file_dialog)
+    b3.pack(side=tkinter.LEFT, padx=5, pady=5)
+
+    master.mainloop()
